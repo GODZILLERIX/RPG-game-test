98b791b6be3b9765b6593bdda318c62d1e62d3be
GODZILLERIX <godzillerix@gmail.com>
2023-06-17 21:22:28 +0200

    test

---
+++ pythonProject2/config.py
@@ -0,0 +1,34 @@
+WIN_WIDTH = 640
+WIN_HEIGHT = 480
+
+FPS = 60
+
+TILESIZE = 32
+
+PLAYER_LAYER = 3
+BLOCK_LAYER = 2
+GROUND_LAYER = 1
+
+PLAYER_SPEED = 3
+
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'B..................B',
+    'B..................B',
+    'B....BBB...........B',
+    'B..................B',
+    'B........P.........B',
+    'B..................B',
+    'B............BBB...B',
+    'B.....BBB..........B',
+    'B.......B..........B',
+    'B.......B..........B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'BBBBBBBBBBBBBBBBBBBB',
+]
